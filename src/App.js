edb55528015f48d619401a99bb46026588585833
Jonathan Ballard <asgardschosen@gmail.com>
2020-05-14 16:37:46 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PersonCard from './components/PersonCard';
+import './App.css';
+
+function App() {
+  const p1 = {
+    first_name: "Jane",
+    last_name: "Doe",
+    age: 45,
+    hair_color: "Black",
+  }
+
+  const p2 = {
+    first_name: "John",
+    last_name: "Smith",
+    age: 88,
+    hair_color: "Brown",
+  }
+
+  const p3 = {
+    first_name: "Millard",
+    last_name: "Fillmore",
+    age: 50,
+    hair_color: "Brown",
+  }
+
+  const p4 = {
+    first_name: "Maria",
+    last_name: "Smith",
+    age: 62,
+    hair_color: "Brown",
+  }
+
+
+  return (
+    <div className="App">
+      < PersonCard person={p1} />
+      < PersonCard person={p2} />
+      < PersonCard person={p3} />
+      < PersonCard person={p4} />
+    </div>
+  );
+}
+
+export default App;
